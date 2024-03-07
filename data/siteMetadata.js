713db7ed0d84d92b059bd6db91482f5d1340b283
etrobot@outlook.com <etrobot@outlook.com>
2024-03-07 18:25:44 +0800

    update style an article

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: 'Frank Lin',
+  headerTitle: 'AICube.FUN',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
