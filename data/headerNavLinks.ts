ac6699d71d7c97da82e40a1f1aa945c5ee0f7ac7
etrobot@outlook.com <etrobot@outlook.com>
2024-03-12 09:52:44 +0800

    add web version chatbot

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: 'https://chat.aicube.fun', title: 'Web-Version' },
   { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
